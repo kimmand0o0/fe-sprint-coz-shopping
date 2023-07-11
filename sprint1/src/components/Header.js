ec494b5f0f4b1c2a7844345b399a7d7e6889a073
kimmand0o0 <yuwang0130@gmail.com>
2023-07-11 19:48:57 +0900

    :bug: Fix : Header 위치 수정
    - Header가 가장 상단에 위치하지 않은 문제 수정
    - Component들에 Styled Component 적용 후 수정
    
    Issues: #3

--- sprint1/src/components/Header.js
+++ sprint1/src/components/Header.js
@@ -6,22 +6,31 @@ import { ReactComponent as LogoName } from "../assets/logoname.svg";
 import { ReactComponent as DropButtonIcon } from "../assets/dropdown-button.svg";
 
 const HeaderBody = styled.div`
-  width: 1280px;
-  hight: 80px;
+  width: 100vw;
+  max-hight: 80px;
 
   display: flex;
   justify-content: space-between;
 
-  padding: 25px 76px 25px 86px;
-
   box-shadow: 0px 8px 8px 0px rgb(0, 0, 0, 0.1);
 
   position: fixed;
+  top: 0;
+`;
+
+const StyledLink = styled(Link)`
+  margin: 25px 0px 25px 76px;
+`;
+
+const LogoImg = styled(Logo)`
+  margin-right: 12px;
 `;
 
 const DropButton = styled.button`
   border: 0;
   background-color: transparent;
+
+  margin-right: 76px;
 
   cursor: pointer;
 `;
@@ -29,10 +38,10 @@ const DropButton = styled.button`
 export default function Header() {
   return (
     <HeaderBody>
-      <Link to="/">
-        <Logo style={{ marginRight: "12px" }} />
+      <StyledLink to="/">
+        <LogoImg />
         <LogoName />
-      </Link>
+      </StyledLink>
       <DropButton>
         <DropButtonIcon />
       </DropButton>
