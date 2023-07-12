f86f0783971f7b9c94a4b84983d664b4e6d8c816
kimmand0o0 <yuwang0130@gmail.com>
2023-07-12 17:13:03 +0900

    :sparkle: Feat : MainPage 뷰 2차 구현
    - axios를 이용하여 데이터 조회
    - axios 사용 시 조건을 걸지 않아 데이터를 무한하게 받아오는 문제 발생 > useState의 길이가 0일 경우라는 조건을 걸어 해결
    - 모든 타입의 상품 정보를 4개 보여준다 (필터기능 없이)
    - 보여지는 상품의 타입은 혼합되어 있을 수 있다 (상품, 카테고리, 기획전, 브랜드)
    
    Issues: #2

--- sprint1/src/pages/Main.js
+++ sprint1/src/pages/Main.js
@@ -1,80 +1,27 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Item from "../components/Item";
-
-const items = [
-  {
-    id: 57,
-    type: "Brand",
-    title: null,
-    sub_title: null,
-    brand_name: "구찌",
-    price: null,
-    discountPercentage: null,
-    image_url: null,
-    brand_image_url:
-      "https://images.unsplash.com/photo-1560519622-7229023b9c86?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80",
-    follower: 8094,
-  },
-  {
-    id: 76,
-    type: "Category",
-    title: "파티용품",
-    sub_title: null,
-    brand_name: null,
-    price: null,
-    discountPercentage: null,
-    image_url:
-      "https://images.unsplash.com/photo-1504196606672-aef5c9cefc92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80",
-    brand_image_url: null,
-    follower: null,
-  },
-  {
-    id: 70,
-    type: "Exhibition",
-    title: "주방 용품 모음전",
-    sub_title: "인덕션 냄비 반값 할인 중",
-    brand_name: null,
-    price: null,
-    discountPercentage: null,
-    image_url:
-      "https://images.unsplash.com/photo-1556912173-46c336c7fd55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-    brand_image_url: null,
-    follower: null,
-  },
-  {
-    id: 67,
-    type: "Product",
-    title: "아이스크림 모자",
-    sub_title: null,
-    brand_name: null,
-    price: "11900",
-    discountPercentage: 50,
-    image_url:
-      "https://images.unsplash.com/photo-1575428652377-a2d80e2277fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    brand_image_url: null,
-    follower: null,
-  },
-];
 
 const MainContainer = styled.div`
   width: 100vw;
 
   padding-top: 80px;
+  padding-bottom: 60px;
 
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const ProductBody = styled.div`
-  background-color: gray;
+const ListSection = styled.section`
   width: 80vw;
 `;
 
-const ProductTitle = styled.h3`
+const ListTitle = styled.h3`
   font-size: 24px;
   line-height: 29.05px;
+  margin: 20px 0px 20px 0px;
 `;
 
 const ProductList = styled.ul`
@@ -85,30 +32,74 @@ const ProductList = styled.ul`
   align-items: center;
 
   padding: 0;
+  margin: 0;
 `;
 
 export default function Main() {
   const [bookmark, setBookmark] = useState([]);
-  const handleClick = (e, id) => {};
+  const [items, setItems] = useState([]);
+
+  const handleClick = (e, id) => {
+    // 북마크가 등록 되어야함.
+    // 등록되어 있는 북마크는 삭제 되어야함.
+    // 알림이 떠야함.
+  };
+
+  useEffect(() => {
+    if (items.length === 0) {
+      axios
+        .get("http://cozshopping.codestates-seb.link/api/v1/products", {
+          params: {
+            count: 10,
+          },
+        })
+        .then((result) => setItems(result.data))
+        .catch(() => {
+          console.log("err");
+        });
+    }
+  }, []);
 
   return (
     <div>
       <MainContainer>
-        <ProductBody>
-          <ProductTitle>상품 리스트</ProductTitle>
+        <ListSection>
+          <ListTitle>상품 리스트</ListTitle>
           <ProductList>
-            {items.map((item, idx) => (
-              <Item
-                item={item}
-                bookmark={bookmark.filter((i) =>
-                  i.id === item.id ? true : false
-                )}
-                key={idx}
-                handleClick={handleClick}
-              />
-            ))}
+            {items
+              .map((item, idx) => (
+                <Item
+                  item={item}
+                  bookmark={bookmark.filter((i) =>
+                    i.id === item.id ? true : false
+                  )}
+                  key={idx}
+                  handleClick={handleClick}
+                />
+              ))
+              .slice(0, 4)}
           </ProductList>
-        </ProductBody>
+        </ListSection>
+        <ListSection>
+          <ListTitle>북마크 리스트</ListTitle>
+          <ProductList>
+            {items
+              .map(
+                (item, idx) =>
+                  bookmark.filter((i) => i.id === item.id) && (
+                    <Item
+                      item={item}
+                      bookmark={bookmark.filter((i) =>
+                        i.id === item.id ? true : false
+                      )}
+                      key={idx}
+                      handleClick={handleClick}
+                    />
+                  )
+              )
+              .slice(0, 4)}
+          </ProductList>
+        </ListSection>
       </MainContainer>
     </div>
   );
