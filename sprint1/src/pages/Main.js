107782f4a75a65cd5fb950beb050850b0099e118
kimmand0o0 <yuwang0130@gmail.com>
2023-07-14 14:56:35 +0900

    :art: Fix : 필요없는 코드 정리

--- sprint1/src/pages/Main.js
+++ sprint1/src/pages/Main.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Item from "../components/Item";
 import Modal from "../components/ItemModal";
